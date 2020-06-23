abf0ff78beec11452a93cee10d6c06661cc4e044
GCoiffier <guillaume.coiffier.42@orange.fr>
2020-06-23 11:24:56 +0200

    fid score test

--- Pytorch/train.py
+++ Pytorch/train.py
@@ -139,7 +139,7 @@ def generate(epoch, generator, N, args, device, exportCuts=False):
     for i in range(N):
         output = generator.generate(1, device).cpu().detach()
         if exportCuts:
-            cuts = extract_3cuts(output).numpy()
+            cuts = extract_3cuts(output).numpy()[0]
             cuts = PILImage.fromarray(cuts)
             cuts.save("output/epoch{}/{}_{}_cuts.png".format(epoch, args.name, i))
         output = output.numpy()
